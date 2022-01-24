c2d87084ff74eaba8318f080ccc1b8fa40716a51
marmarj2 <marmarj2@inf.upv.es>
2022-01-24 17:20:32 +0100

    Funcionalidad completa de Google Maps

--- ExtractorIEI/WrapperCV.cs
+++ ExtractorIEI/WrapperCV.cs
@@ -15,7 +15,8 @@ namespace Extractor
 {
     public class WrapperCV
     {
-        static readonly ChromeDriver driver = new ChromeDriver();        
+        static readonly ChromeDriver driver = new ChromeDriver();
+        static IWebElement webElement = null;
 
         public static void ExtractCV(string originFilePath, string jsonDestinyFilePath, string destinyFilePath)
         {
@@ -63,19 +64,6 @@ namespace Extractor
                 bibliotecaGEN.codigoProvincia = biblioteca.COD_PROVINCIA; //hecho
                 try
                 {
-                    /*
-                    driver.FindElement(By.Id("address")).Clear();
-                    Thread.Sleep(1000);
-                    driver.FindElement(By.Id("address")).SendKeys(bibliotecaGEN.direccion.Split("Nº")[0] + ", " + bibliotecaGEN.codigoPostal + ", España");
-                    Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//button[text()= " + '\u0022'.ToString() + "Obtener Coordenadas GPS" + '\u0022'.ToString() + "]")).Click();
-                    Thread.Sleep(1000);
-
-                    
-                    bibliotecaGEN.longitud = driver.FindElement(By.Id("latitude")).GetAttribute("value");
-                    bibliotecaGEN.latitud = driver.FindElement(By.Id("longitude")).GetAttribute("value");
-                    */
-
                     string[] coordenadas = buscarCoordenadasGPS(bibliotecaGEN);
                     bibliotecaGEN.longitud = coordenadas[0];
                     bibliotecaGEN.latitud = coordenadas[1];
@@ -102,10 +90,17 @@ namespace Extractor
             driver.Url = "https://maps.google.com";
             driver.ExecuteScript("window.scrollBy(0,1000)");
 
-            driver.FindElement(By.XPath("/html/body/c-wiz/div/div/div/div[2]/div/div[4]/form/div"))?.Click();
-            Thread.Sleep(1000);
-            driver.FindElement(By.XPath("html/body/div[3]/div[9]/div[2]/div/div/div/div[2]/form/")).SendKeys(bibliotecaGEN.direccion.Split("Nº")[0] + ", " + bibliotecaGEN.codigoPostal + ", España" + Keys.Enter);
+            if (webElement is null)
+            {
+                webElement = driver.FindElement(By.XPath("/html/body/c-wiz/div/div/div/div[2]/div/div[4]/form/div"));
+                webElement.Click();
+            }
 
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/form/div/div[3]/div/input[1]")).SendKeys(bibliotecaGEN.direccion.Split("Nº")[0] + ", " + bibliotecaGEN.codigoPostal + ", España");
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button"))?.Click();
+            Thread.Sleep(1000);
             string[] coordenadas = new string[2];
             coordenadas[0] = driver.Url.Split('@')[1].Split(',')[0];
             coordenadas[1] = driver.Url.Split('@')[1].Split(',')[1].Split('/')[0];
