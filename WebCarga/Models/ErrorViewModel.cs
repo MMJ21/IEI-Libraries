b4e5dae5943ec19d40c0dd3b598e862a2092b358
marmarj2 <marmarj2@inf.upv.es>
2022-01-25 21:14:55 +0100

    Web Carga

---
+++ WebCarga/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebCarga.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
