7bdc6f13b7cd6b153519e0f1f689642984b55383
marmarj2 <marmarj2@inf.upv.es>
2022-01-25 22:18:28 +0100

    API Busqueda

---
+++ APIBusqueda/Biblioteca.cs
@@ -0,0 +1,15 @@
+namespace APIBusqueda
+{
+    public class Biblioteca
+    {
+        public string nombre { get; set; }
+		public string tipo { get; set; }
+		public string direccion { get; set; }
+		public string codigoPostal { get; set; }
+		public string longitud { get; set; }
+		public string latitud { get; set; }
+		public string telefono { get; set; }
+		public string email { get; set; }
+		public string descripcion { get; set; }
+    }
+}
--- APIBusqueda/Controllers/BusquedaController.cs
+++ APIBusqueda/Controllers/BusquedaController.cs
@@ -1,33 +1,75 @@
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace APIBusqueda.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/bibliotecas")]
     public class BusquedaController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public MySqlConnection connector;
+        public void conectionToDB()
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            // Configuring the DB Connetion
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.Database = "mybiblioteca";
+            builder.UserID = "root";
+            builder.Password = "root";
 
-        private readonly ILogger<BusquedaController> _logger;
-
-        public BusquedaController(ILogger<BusquedaController> logger)
-        {
-            _logger = logger;
+            connector = new MySqlConnection(builder.ToString());
+            connector.Open();
         }
 
         [HttpGet(Name = "GetBibliotecas")]
-        public IEnumerable<Busqueda> Get()
+        public IActionResult Get(string localidad, string cP, string provincia, string tipo)
         {
-            return Enumerable.Range(1, 5).Select(index => new Busqueda
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            try{
+                conectionToDB(); //Initializing DB Connection
+            } catch(Exception e){ console.WriteLine(e.getMessage()); return null;}
+
+            string query = "SELECT b.nombre, b.tipo, b.direccion, b.codigoPostal, b.longitud, b.latitud, b.telefono, b.email,b.descripcion FROM mybiblioteca.Biblioteca b " +
+                "JOIN mybiblioteca.Localidad l ON (b.codigoLocalidad = l.codigo) " +
+                "JOIN mybiblioteca.Provincia p ON (l.codigoProvincia = p.codigo)  " +
+                "WHERE 1 = 1 ";
+
+            if (localidad != null) { query += "AND TRIM(UCASE(b.codigoLocalidad)) = TRIM(UCASE(" + localidad + ")) "; }
+            if (cP != null) { query += " AND TRIM(UCASE(b.codigoPostal)) = TRIM(UCASE(" + cP + ")) "; }
+            if (provincia != null) { query += " AND TRIM(UCASE(l.codigoProvincia)) = TRIM(UCASE(" + provincia + ")) "; }
+            if (tipo != null) { query += "AND TRIM(UCASE(b.tipo)) = TRIM(UCASE(" + tipo + ")) "; }
+
+            query += ";";
+
+            using var command = new MySqlCommand(query, connector);
+            using var reader = command.ExecuteReader();
+            
+            string jsonString = '';
+
+            if (reader.HasRows){
+                while (reader.Read()){
+                    
+                    var biblioteca = new Biblioteca(
+                            nombre = reader.GetString(0)
+                            tipo = reader.GetString(1)
+                            direccion = reader.GetString(2)
+                            codigoPostal = reader.GetString(3)
+                            longitud = reader.GetString(4)
+                            latitud = reader.GetString(5)
+                            telefono = reader.GetString(6)
+                            email = reader.GetString(7)
+                            descripcion = reader.GetString(8)
+                    );
+
+                    jsonString += JsonSerializer.Serialize(biblioteca)
+   
+                }
+            }
+
+            reader.Close();
+            return Ok(jsonString);
+   
         }
     }
 }
