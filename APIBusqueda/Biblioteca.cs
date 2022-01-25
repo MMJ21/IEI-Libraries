1bb790eaf005e257b295beb91f1eb187d30e6121
marmarj2 <marmarj2@inf.upv.es>
2022-01-26 00:45:44 +0100

    Llamada Post

--- APIBusqueda/Biblioteca.cs
+++ APIBusqueda/Biblioteca.cs
@@ -11,5 +11,18 @@ namespace APIBusqueda
 		public string telefono { get; set; }
 		public string email { get; set; }
 		public string descripcion { get; set; }
+
+        public Biblioteca(string nombre, string tipo, string direccion, string codigoPostal, string longitud, string latitud, string telefono, string email, string descripcion)
+        {
+            this.nombre = nombre;
+            this.tipo = tipo;
+            this.direccion = direccion;
+            this.codigoPostal = codigoPostal;
+            this.longitud = longitud;
+            this.latitud = latitud;
+            this.telefono = telefono;
+            this.email = email;
+            this.descripcion = descripcion;
+        }
     }
 }
--- APIBusqueda/Controllers/BusquedaController.cs
+++ APIBusqueda/Controllers/BusquedaController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -23,53 +25,71 @@ namespace APIBusqueda.Controllers
             connector.Open();
         }
 
-        [HttpGet(Name = "GetBibliotecas")]
-        public IActionResult Get(string localidad, string cP, string provincia, string tipo)
+        [HttpPost(Name = "GetBibliotecas")]
+        public IActionResult Get(Rootobject data)
         {
+
             try{
                 conectionToDB(); //Initializing DB Connection
-            } catch(Exception e){ console.WriteLine(e.getMessage()); return null;}
+            } catch(Exception e){ Console.WriteLine(e.Message); return null;}
+
 
             string query = "SELECT b.nombre, b.tipo, b.direccion, b.codigoPostal, b.longitud, b.latitud, b.telefono, b.email,b.descripcion FROM mybiblioteca.Biblioteca b " +
                 "JOIN mybiblioteca.Localidad l ON (b.codigoLocalidad = l.codigo) " +
-                "JOIN mybiblioteca.Provincia p ON (l.codigoProvincia = p.codigo)  " +
-                "WHERE 1 = 1 ";
+                "JOIN mybiblioteca.Provincia p ON (l.codigoProvincia = p.codigo) WHERE 1 = 1 ";
 
-            if (localidad != null) { query += "AND TRIM(UCASE(b.codigoLocalidad)) = TRIM(UCASE(" + localidad + ")) "; }
-            if (cP != null) { query += " AND TRIM(UCASE(b.codigoPostal)) = TRIM(UCASE(" + cP + ")) "; }
-            if (provincia != null) { query += " AND TRIM(UCASE(l.codigoProvincia)) = TRIM(UCASE(" + provincia + ")) "; }
-            if (tipo != null) { query += "AND TRIM(UCASE(b.tipo)) = TRIM(UCASE(" + tipo + ")) "; }
+            if (data.localidad != "") { query += String.Format("AND TRIM(UCASE('{0}')) = TRIM(UCASE(l.nombre)) ", data.localidad ); }
+            if (data.cP != "") { query += String.Format("AND TRIM(UCASE(b.codigoPostal)) = TRIM(UCASE('{0}')) ",data.cP); }
+            if (data.provincia != "") { query += String.Format("AND TRIM(UCASE(p.nombre)) = TRIM(UCASE('{0}')) ",data.provincia); }
+            if (data.tipo != "") { query += String.Format("AND TRIM(UCASE(b.tipo)) = TRIM(UCASE('{0}')) ",data.tipo); }
 
             query += ";";
 
             using var command = new MySqlCommand(query, connector);
             using var reader = command.ExecuteReader();
-            
-            string jsonString = '';
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            string jsonString = "";
 
             if (reader.HasRows){
                 while (reader.Read()){
                     
-                    var biblioteca = new Biblioteca(
-                            nombre = reader.GetString(0)
-                            tipo = reader.GetString(1)
-                            direccion = reader.GetString(2)
-                            codigoPostal = reader.GetString(3)
-                            longitud = reader.GetString(4)
-                            latitud = reader.GetString(5)
-                            telefono = reader.GetString(6)
-                            email = reader.GetString(7)
-                            descripcion = reader.GetString(8)
+                    Biblioteca biblioteca = new Biblioteca(
+                            reader.GetString(0),
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetString(3),                            
+                            reader.GetString(4),
+                            reader.GetString(5),
+                            reader.GetString(6),
+                            reader.GetString(7),
+                            reader.GetString(8)
                     );
 
-                    jsonString += JsonSerializer.Serialize(biblioteca)
-   
+                    jsonString += JsonSerializer.Serialize(biblioteca, options)+",";
+                    jsonString.TrimEnd('\n');
                 }
             }
-
+           
             reader.Close();
-            return Ok(jsonString);
+            connector.Close();
+
+            return new JsonResult(jsonString);
+
+            
    
+        }
+
+        public class Rootobject
+        { 
+           public string localidad { get; set; }
+           public string cP { get; set; }
+           public string provincia { get; set; }
+           public string tipo { get; set; }
         }
     }
 }
